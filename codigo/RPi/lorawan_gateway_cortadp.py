afd1c7853e21bd1cc5cf59649b6b7a91335a527a
DavidFigCas <david.fc@inventeoca.com>
2021-01-15 10:52:42 -0600

    editar script gateway

--- codigo/RPi/lorawan_gateway_cortadp.py
+++ codigo/RPi/lorawan_gateway_cortadp.py
@@ -1,7 +1,7 @@
 """
 Raspberry Pi Single Channel Gateway
 
-Learn Guide: https://learn.adafruit.com/raspberry-pi-single-channel-lorawan-gateway
+Learn Guide: https://learn.adafruit.com/raspberry-pi-single-channel-lorawan-g$
 Author: Brent Rubell for Adafruit Industries
 """
 # Import Python System Libraries
@@ -14,7 +14,7 @@ import busio
 from digitalio import DigitalInOut, Direction, Pull
 import board
 # Import the SSD1306 module.
-import adafruit_ssd1306
+#import adafruit_ssd1306
 
 # Button A
 btnA = DigitalInOut(board.D5)
@@ -32,20 +32,21 @@ btnC.direction = Direction.INPUT
 btnC.pull = Pull.UP
 
 # Create the I2C interface.
-i2c = busio.I2C(board.SCL, board.SDA)
+#i2c = busio.I2C(board.SCL, board.SDA)
+
 # 128x32 OLED Display
-display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+#display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
 # Clear the display.
-display.fill(0)
-display.show()
-width = display.width
-height = display.height
+#display.fill(0)
+#display.show()
+#width = display.width
+#height = display.height
 
 # Gateway id calculation (based off MAC address)
 mac_addr = hex(uuid.getnode()).replace('0x', '')
-print('Gateway ID: {0}:{1}:{2}:ff:ff:{3}:{4}:{5}'.format(mac_addr[0:2],mac_addr[2:4],
-                                                         mac_addr[4:6],mac_addr[6:8],
-                                                         mac_addr[8:10], mac_addr[10:12]))
+print('Gateway ID: {0}:{1}:{2}:ff:ff:{3}:{4}:{5}'.format(mac_addr[0:2],mac_ad$
+                                                         mac_addr[4:6],mac_ad$
+                                                         mac_addr[8:10], mac_$
 
 # Parse `global_conf.json`
 with open('global_conf.json', 'r') as config:
@@ -68,21 +69,21 @@ def stats():
     """
     print('MODE: Pi Stats')
     # Clear Display
-    display.fill(0)
+    #display.fill(0)
     # Shell scripts for system monitoring from here :
-    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    # https://unix.stackexchange.com/questions/119126/command-to-display-memo$
     cmd = "hostname -I | cut -d\' \' -f1"
     IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
     cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
     CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*10$
     MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
     # write text to display
-    display.text("IP: "+str(IP), 0, 0, 1)
-    display.text(str(CPU), 0, 15, 1)
-    display.text(str(MemUsage), 0, 25, 1)
+    #display.text("IP: "+str(IP), 0, 0, 1)
+    #display.text(str(CPU), 0, 15, 1)
+    #display.text(str(MemUsage), 0, 25, 1)
     # display text for 3 seconds
-    display.show()
+    #display.show()
     time.sleep(3)
 
 def gateway():
@@ -91,34 +92,34 @@ def gateway():
     """
     print('MODE: Pi Gateway')
     # Clear Display
-    display.fill(0)
-    display.text("Starting Gateway...", 15, 0, 1)
-    display.show()
+    #display.fill(0)
+    #display.text("Starting Gateway...", 15, 0, 1)
+    #display.show()
     print('starting gateway...')
     try:
         proc = subprocess.Popen("./single_chan_pkt_fwd",
-                                bufsize=-1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                                bufsize=-1, stdout=subprocess.PIPE, stderr=su$
     except FileNotFoundError:
-        print("To run the single packet forwarder, you'll need to run `sudo make all` first.")
+        print("To run the single packet forwarder, you'll need to run `sudo m$
         return
-    display.fill(0)
-    display.text(gateway_name, 15, 0, 1)
-    display.show()
+    #display.fill(0)
+    #display.text(gateway_name, 15, 0, 1)
+    #display.show()
     while True:
         new_line = proc.stdout.readline().decode('utf-8')
         print(new_line)
         # grab new data on gateway status update
         if new_line == "gateway status update\n":
-            display.fill(0)
+            #display.fill(0)
             gtwy_timestamp = proc.stdout.readline().decode('utf-8')
             print('time:', gtwy_timestamp)
             gtwy_status = proc.stdout.readline().decode('utf-8')
             print(gtwy_status)
-            display.text(gateway_name, 15, 0, 1)
-            display.text(gtwy_status, 0, 15, 1)
-            display.text(gtwy_timestamp[11:23], 25, 25, 1)
+            #display.text(gateway_name, 15, 0, 1)
+            #display.text(gtwy_status, 0, 15, 1)
+            #display.text(gtwy_timestamp[11:23], 25, 25, 1)
         elif new_line == "incoming packet...\n":
-            display.fill(0)
+            #display.fill(0)
             print('incoming pkt...')
             # read incoming packet info
             pkt_json = proc.stdout.readline().decode('utf-8')
@@ -134,35 +135,37 @@ def gateway():
             pkt_size = pkt_data['size']
             pkt_rssi = pkt_data['rssi']
             pkt_tmst = pkt_data['tmst']
-            display.text('* PKT RX on {0}MHz'.format(pkt_freq), 0, 0, 1)
-            display.text('RSSI: {0}dBm, Sz: {1}b'.format(pkt_rssi, pkt_size), 0, 10, 1)
-            display.text('timestamp: {0}'.format(pkt_tmst), 0, 20, 1)
-        display.show()
+            #display.text('* PKT RX on {0}MHz'.format(pkt_freq), 0, 0, 1)
+            #display.text('RSSI: {0}dBm, Sz: {1}b'.format(pkt_rssi, pkt_size)$
+            #display.text('timestamp: {0}'.format(pkt_tmst), 0, 20, 1)
+        #display.show()
 
 def gateway_info():
     """Displays information about the LoRaWAN gateway.
     """
     print('MODE: Gateway Info')
-    display.fill(0)
-    display.show()
+    #display.fill(0)
+    #display.show()
     print('Server: ', ttn_server_addr[0:9])
     print('Freq: ', gateway_freq)
     print('SF: ', gateway_sf)
     print('Gateway Name:', gateway_name)
     # write 3 lines of text
-    display.text(gateway_name, 15, 0, 1)
-    display.text('{0} MHz, SF{1}'.format(gateway_freq, gateway_sf), 15, 10, 1)
-    display.text('TTN: {0}'.format(ttn_server_addr[0:9]), 15, 20, 1)
-    display.show()
+    #display.text(gateway_name, 15, 0, 1)
+    #display.text('{0} MHz, SF{1}'.format(gateway_freq, gateway_sf), 15, 10, $
+    #display.text('TTN: {0}'.format(ttn_server_addr[0:9]), 15, 20, 1)
+    #display.show()
     time.sleep(3)
+
+
 while True:
     # draw a box to clear the image
-    display.fill(0)
-    display.text('LoRaWAN Gateway EUI', 15, 0, 1)
-    display.text('{0}:{1}:{2}:ff'.format(mac_addr[0:2], mac_addr[2:4],
-                                         mac_addr[4:6]), 25, 15, 1)
-    display.text('ff:{0}:{1}:{2}'.format(mac_addr[6:8],mac_addr[8:10],
-                                         mac_addr[10:12]), 25, 25, 1)
+    #display.fill(0)
+    #display.text('LoRaWAN Gateway EUI', 15, 0, 1)
+    #display.text('{0}:{1}:{2}:ff'.format(mac_addr[0:2], mac_addr[2:4],
+    #                                     mac_addr[4:6]), 25, 15, 1)
+    #display.text('ff:{0}:{1}:{2}'.format(mac_addr[6:8],mac_addr[8:10],
+    #                                     mac_addr[10:12]), 25, 25, 1)
 
     # Radio Bonnet Buttons
     if not btnA.value:
@@ -175,5 +178,5 @@ while True:
         # show gateway configuration
         gateway_info()
 
-    display.show()
+    #display.show()
     time.sleep(.1)
